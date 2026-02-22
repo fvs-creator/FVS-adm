7a78942b6d20b2a6391e026d8ec810d7fb6997e5
fvs-creator <vincejoshuatiktok@gmail.com>
2026-02-22 21:48:46 +0800

    Update universal.lua

--- universal.lua
+++ universal.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/eIysia-dev/release/refs/heads/main/open-src.lua"))()
-loadstring(game:HttpGet("https://pastefy.app/je3gk5AF/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/rkRuhluQ/raw"))()
